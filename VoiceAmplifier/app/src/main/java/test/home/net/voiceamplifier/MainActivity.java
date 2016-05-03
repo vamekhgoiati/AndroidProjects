0e3edbeffe73387f1867ca7a012cf56c2a877202
Vamekh Goiati <vamekh.goiati@gmail.com>
2016-05-03 14:50:22 +0400

    space master

--- VoiceAmplifier/app/src/main/java/test/home/net/voiceamplifier/MainActivity.java
+++ VoiceAmplifier/app/src/main/java/test/home/net/voiceamplifier/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        
+
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
