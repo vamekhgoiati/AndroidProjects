2c779d25d12b164e162804b6e2cfbcc8bf06615c
Vamekh Goiati <vamekh.goiati@gmail.com>
2016-05-07 01:28:59 +0400

    Added seekbar for gain control 0 - 3, removed unused buttons, added configuration change ignore in manifest

--- VoiceAmplifier/app/src/main/java/test/home/net/voiceamplifier/MainActivity.java
+++ VoiceAmplifier/app/src/main/java/test/home/net/voiceamplifier/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,76 +21,105 @@ public class MainActivity extends AppCompatActivity {
     private static final int OUT_CHANNEL = AudioFormat.CHANNEL_OUT_MONO;
     private static final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 
+    // values from [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+    //               0    1    2    3    4    5    6
+    private static final int SEEKBAR_MAX_VALUE = 6;
+    private static final int SEEKBAR_START_POS = 2;
+    private static final float SEEKBAR_STEP = .5f;
+
     private float gain = 1.0f;
 
     private Button startByteBufferButton;
-    private Button startByteBufferGainButton;
-    private Button startShortBufferButton;
-    private Button startShortBufferGain1Button;
-    private Button startShortBufferGain2Button;
     private Button stopButton;
+    private SeekBar gainSeekBar;
+
     private boolean isRecording;
+    private boolean continueRec;
     private AudioTrack audioTrack;
     private AudioRecord audioRecord;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         startByteBufferButton = (Button) findViewById(R.id.start_byte_buffer_button);
-        startByteBufferGainButton = (Button) findViewById(R.id.start_byte_buffer_gain_button);
-        startShortBufferButton = (Button) findViewById(R.id.start_short_buffer_button);
-        startShortBufferGain1Button = (Button) findViewById(R.id.start_short_buffer_gain1_button);
-        startShortBufferGain2Button = (Button) findViewById(R.id.start_short_buffer_gain2_button);
         stopButton = (Button) findViewById(R.id.stop_button);
+        gainSeekBar = (SeekBar) findViewById(R.id.gain_seek_bar);
+
+        gainSeekBar.setMax(SEEKBAR_MAX_VALUE);
+        gainSeekBar.setProgress(SEEKBAR_START_POS);
 
         stopButton.setEnabled(false);
 
         setListeners();
 
+        if (savedInstanceState != null) {
+            Log.i(TAG, "onCreate() called, activity re-created");
+        } else {
+            Log.i(TAG, "onCreate() called, new activity");
+        }
+
     }
 
     private void setListeners() {
 
-        View.OnClickListener listener = new View.OnClickListener() {
+        startByteBufferButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isRecording) {
                     isRecording = true;
                     setButtonsEnabled(isRecording);
-                    createBufferThreadAndStart(v.getId());
+                    createByteBufferThread().start();
                 }
             }
-        };
-
-        startByteBufferButton.setOnClickListener(listener);
-        startByteBufferGainButton.setOnClickListener(listener);
-        startShortBufferButton.setOnClickListener(listener);
-        startShortBufferGain1Button.setOnClickListener(listener);
-        startShortBufferGain2Button.setOnClickListener(listener);
+        });
 
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 isRecording = false;
                 setButtonsEnabled(isRecording);
-                releaseAudioResources();
+                releaseAudioResources(true);
+            }
+        });
+
+        gainSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                gain = getSeekBarValue(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
     }
 
+    private float getSeekBarValue(int intValue) {
+        float res;
+        res = 1.0f + (intValue - SEEKBAR_START_POS) * SEEKBAR_STEP;
+        return res;
+    }
+
     private Thread createByteBufferThread() {
-        Thread t = new Thread(new Runnable() {
+
+        return new Thread(new Runnable() {
             @Override
             public void run() {
                 int bufferSize = initAudioComponents();
                 startByteBuffer(bufferSize);
             }
         });
-
-        return t;
     }
 
     private void startByteBuffer(int bufferSize) {
@@ -125,126 +155,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void createBufferThreadAndStart(int id) {
-        switch (id) {
-            case R.id.start_byte_buffer_button:
-                gain = 1.0f;
-                createByteBufferThread().start();
-                break;
-            case R.id.start_byte_buffer_gain_button:
-                gain = 3.0f;
-                createByteBufferThread().start();
-                break;
-            case R.id.start_short_buffer_button:
-                gain = 1.0f;
-                createShortBufferThread().start();
-                break;
-            case R.id.start_short_buffer_gain1_button:
-                gain = 2.0f;
-                createShortBufferThread().start();
-                break;
-            case R.id.start_short_buffer_gain2_button:
-                gain = 4.0f;
-                createShortBufferThread().start();
-                break;
-        }
-    }
-
-    private Thread createShortBufferThread() {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int bufferSize = initAudioComponents();
-                startShortBuffer(bufferSize);
-            }
-        });
-
-        return t;
-    }
-
-    private void startShortBuffer(int bufferSize) {
-        if (audioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
-            audioRecord.startRecording();
-            audioTrack.play();
-
-            byte[] byteBuffer = new byte[bufferSize];
-            short[] buffer;
-            while (isRecording) {
-
-                int readSize = audioRecord.read(byteBuffer, 0,
-                        byteBuffer.length);
-
-                Log.i(TAG, "Size read: " + readSize);
-                buffer = byteArray2ShortArray(byteBuffer);
-
-                for (int frameIndex = 0; frameIndex < buffer.length; frameIndex++) {
-                    if (gain != 1) {
-
-                        double sample = ((double) buffer[frameIndex] / Double.MAX_VALUE);
-                        sample *= gain;
-                        int intValue = (int) (sample * Double.MAX_VALUE);
-
-                        if (intValue > 32765) {
-                            intValue = 32765;
-                        }
-
-                        if (intValue < -32767) {
-                            intValue = -32767;
-                        }
-
-                        buffer[frameIndex] = (short) intValue;
-                    }
-                }
-
-                audioTrack.write(shortArray2ByteArray(buffer), 0, buffer.length * 2);
-            }
-
-        } else {
-            Log.i(TAG, "Could not initialize AudioRecord object. Application will shut down");
-            finish();
-        }
-    }
-
     private void setButtonsEnabled(boolean isRecording) {
         stopButton.setEnabled(isRecording);
         startByteBufferButton.setEnabled(!isRecording);
-        startByteBufferGainButton.setEnabled(!isRecording);
-        startShortBufferButton.setEnabled(!isRecording);
-        startShortBufferGain1Button.setEnabled(!isRecording);
-        startShortBufferGain2Button.setEnabled(!isRecording);
-    }
-
-    private byte[] shortArray2ByteArray(short[] shortArray) {
-        byte[] byteArray = new byte[shortArray.length * 2];
-        byte[] tempBuffer;
-        for (int i = 0; i < shortArray.length; i++) {
-            tempBuffer = short2Byte(shortArray[i]);
-            byteArray[i * 2] = tempBuffer[0];
-            byteArray[(i * 2) + 1] = tempBuffer[1];
-        }
-
-        return byteArray;
-    }
-
-    private short[] byteArray2ShortArray(byte[] byteArray) {
-        short[] shortArray = new short[byteArray.length / 2];
-        for (int i = 0; i < byteArray.length / 2; i++) {
-            shortArray[i] = byte2Short(byteArray[i * 2], byteArray[(i * 2) + 1]);
-        }
-
-        return shortArray;
-    }
-
-    private short byte2Short(byte b1, byte b2) {
-        return (short) ((b1 & 0xff) | (b2 << 8));
-    }
-
-    private byte[] short2Byte(short s) {
-        byte[] res = new byte[2];
-        res[0] = (byte) (s & 0xff);
-        res[1] = (byte) ((s >> 8) & 0xff);
-
-        return res;
     }
 
     private int initAudioComponents() {
@@ -256,15 +169,14 @@ public class MainActivity extends AppCompatActivity {
         audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, RATE, OUT_CHANNEL, ENCODING, recordBufferSize, AudioTrack.MODE_STREAM);
 
         return recordBufferSize;
+
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseAudioResources();
-    }
+    private void releaseAudioResources(boolean stopRecording) {
 
-    private void releaseAudioResources() {
+        if (stopRecording) {
+            isRecording = false;
+        }
 
         if (audioRecord != null && audioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
             audioRecord.release();
@@ -273,17 +185,44 @@ public class MainActivity extends AppCompatActivity {
         if (audioTrack != null && audioTrack.getState() == AudioTrack.STATE_INITIALIZED) {
             audioTrack.release();
         }
+
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        releaseAudioResources();
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called");
+        if (continueRec) {
+            isRecording = true;
+            createByteBufferThread().start();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        releaseAudioResources();
+        Log.i(TAG, "onPause() called");
+        continueRec = isRecording;
+        releaseAudioResources(true);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop() called");
+        releaseAudioResources(true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy() called");
+        releaseAudioResources(true);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState(...) called");
     }
 }
--- VoiceAmplifier/app/src/test/java/test/home/net/voiceamplifier/ExampleUnitTest.java
+++ VoiceAmplifier/app/src/test/java/test/home/net/voiceamplifier/ExampleUnitTest.java
@@ -12,4 +12,94 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void arrayTest() {
+        short s = 10; //0000000000001010
+        byte b1,b2;
+
+        b1 = (byte) (s & 0xff);
+        b2 = (byte) ((s >> 8) & 0xff);
+
+        System.out.println((short) (s & 0xff));
+        System.out.println((short) ((s >> 8) & 0xff));
+        System.out.println(b1);
+        System.out.println(b2);
+
+        String str = "";
+        for (int i = 0; i < 16; i++) {
+            str = (s & 1) + str;
+            s = (short) (s >> 1);
+            System.out.println(str);
+            System.out.println(s);
+        }
+
+        long a = 16;
+        long b = (a >> 2);
+
+        System.out.println(b);
+
+        short t = 11;
+        byte[] tB = short2Byte(t);
+        System.out.println(byte2Short(tB[0], tB[1]));
+
+    }
+
+    @Test
+    public void EndiannessTest() {
+        int i = 10;
+        byte[] b = new byte[2];
+        b[0] = (byte) (i & 0xff);
+        b[1] = (byte) ((i >> 8) & 0xff);
+
+        double d1 = (double) ((b[0] << 8) | (b[1] & 0xff));
+        double d2 = (double) ((b[0] & 0xff) | (b[1] << 8));
+
+        System.out.println("d1 " + d1);
+        System.out.println("d2 " + d2);
+    }
+
+    private short byte2Short(byte b1, byte b2) {
+        return (short) ((b1 & 0xff) | (b2 << 8));
+    }
+
+    @Test
+    public void wtf() {
+        short[] s = new short[]{667, 776};
+        long acc = 0;
+
+        acc += ((long)(s[0] & 0xff)) << 48;
+        acc += ((long)(s[1] & 0xff)) << 56;
+
+        double sample = ((double) acc / (double) Long.MAX_VALUE);
+        sample *= 2;
+        int intValue = (int) (sample * (double) Integer.MAX_VALUE);
+
+        for (int i = 0; i < 2; i++) {
+            s[i] = (byte) (intValue >>> ((i + 2) * 8) & 0xff);
+        }
+
+        System.out.println("s[0] = " + s[0]);
+        System.out.println("s[1] = " + s[1]);
+
+    }
+
+    @Test
+    public void shiftTest() {
+        int a = -10;
+        System.out.println(Integer.toBinaryString(a >>> 1));
+        System.out.println(a >>> 1);
+        System.out.println(Integer.toBinaryString(a >> 1));
+        System.out.println(a >> 1);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    private byte[] short2Byte(short s) {
+        byte[] res = new byte[2];
+        res[0] = (byte) (s & 0xff);
+        res[1] = (byte) ((s >> 8) & 0xff);
+
+        return res;
+    }
 }
